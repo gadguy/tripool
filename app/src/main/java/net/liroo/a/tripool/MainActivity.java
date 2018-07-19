b978adbfec650a2a6d228e95e4d1ea54356892d0
wonjehkim <wonjeh@gmail.com>
2018-07-19 18:01:42 +0900

    login, logout done!

--- app/src/main/java/net/liroo/a/tripool/MainActivity.java
+++ app/src/main/java/net/liroo/a/tripool/MainActivity.java
@@ -42,8 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
